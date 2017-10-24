3e888277c65923b276b82f30190ecf64104c238c
bgirlcoolcat <evansb21@gmail.com>
2017-10-24 16:52:39 +0100

    lyrics data for hardcoding into game

---
+++ lyricsData.js
@@ -0,0 +1,54 @@
+//One line of lyrics from ten all-time artists
+
+const data = [
+    {
+        artist: "Beyoncé", 
+        song: "Halo",
+        lyrics: "Remember those walls I built?\nWell, baby they're tumbling down\nAnd they didn't even put up a fight\nThey didn't even make a sound"
+    }, 
+    {
+        artist: "David Bowie",
+        song: "Starman",
+        lyrics: "There's a starman waiting in the sky\nHe'd like to come and meet us\nBut he thinks he'd blow our minds"    
+    },
+    {
+        artist: "Taylor Swift",
+        song: "Shake It Off",
+        lyrics: "I stay out too late\nGot nothing in my brain\nThat's what people say, mm-mm\nThat's what people say, mm-mm"
+    },
+    {
+        artist: "Take That",
+        song: "Rule the World",
+        lyrics: "You light the skies, up above me\nA star, so bright, you blind me"
+    },
+    {
+        artist: "LMFAO",
+        song: "Rock the World",
+        lyrics: "Party rock is in the house tonight\nEverybody just have a good time\nAnd we gon' make you loose your mind (wooo!)"
+    },
+    {
+        artist: "The Beatles",
+        song: "Hey Jude",
+        lyrics: "Hey Jude, don't make it bad\nTake a sad song and make it better"
+    },
+    {
+        artist: "Diana Ross and Lionel Ritchie", 
+        song: "Endless Love",
+        lyrics: "My love\nThere's only you in my life\nThe only thing that's bright\n\nMy first love"
+    },
+    {
+        artist: "Bryan Adams",
+        song: "(Everything I Do) I Do It For You",
+        lyrics: "Look into my eyes \n you will see\nWhat you mean to me.\nSearch your heart, search your soul\nAnd when you find me there you'll search no more."
+    },
+    {
+        artist: "Adele",
+        song: "Rolling In The Deep",
+        lyrics: "There's a fire starting in my heart\nReaching a fever pitch and it's bring me out the dark"
+    },
+    {
+        artist: "Frank Sinatra",
+        song: "New York, New York",
+        lyrics: "Start spreading the news\nI am leaving today"
+    }
+];
