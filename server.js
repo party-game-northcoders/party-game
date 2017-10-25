c57cac89b1e743a455da34fca35f2f09286b7c96
bgirlcoolcat <evansb21@gmail.com>
2017-10-25 16:02:10 +0100

    fetch lyrics against randomised artist/song array and parse out data as a json object

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const axios = require('axios');
+const songs = require('./songList');
 
 app.listen(3000);
 
@@ -21,19 +22,41 @@ app.use(bodyParser.json())
 
 // })
 
-app.get("/:song/:artist", function(req, res) {
-    const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${req.params.song}&q_artist=${req.params.artist}&apikey=c6af8e74da168c2f810eab97f6a8f603`; 
+let title = "";
+let singer= "";
+function songRandomiser(arr) {
+    let random = Math.floor(Math.random() * songs.length)
+    title = arr[random].song 
+    singer = arr[random].artist 
+}
+
+
+
+app.get("/random_song", function(req, res) {
+    
+    songRandomiser(songs);
+    
+    const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${title}&q_artist=${singer}&apikey=c6af8e74da168c2f810eab97f6a8f603`; 
     
     return axios
         .get(url)
         .then(response => {
             // res.send(response.data.message.body)
-            res.send(response.data.message.body.lyrics.lyrics_body)
+            let lyrics = response.data.message.body.lyrics.lyrics_body
+
+            //take first 3 new lines 
+            let lyricsArray = lyrics.split("\n");
+            let lyricsOutput = lyricsArray.slice(0, 3).join("/n");
+
+            res.send({
+                title: title,
+                singer: singer,
+                lyricsOutput: lyricsOutput 
+            })
         }
         )
         .catch(e => res.send(e.message));
 
 })
-
 
 
---
+++ songList.js
@@ -0,0 +1,44 @@
+const songs = [
+    {
+        artist: "Beyoncé", 
+        song: "Halo",
+    }, 
+    {
+        artist: "David Bowie",
+        song: "Starman",
+},
+    {
+        artist: "Taylor Swift",
+        song: "Shake It Off",
+},
+    {
+        artist: "Take That",
+        song: "Rule the World",
+},
+    {
+        artist: "LMFAO",
+        song: "Rock the World",
+},
+    {
+        artist: "The Beatles",
+        song: "Hey Jude",
+},
+    {
+        artist: "Diana Ross and Lionel Ritchie", 
+        song: "Endless Love",
+ },
+    {
+        artist: "Bryan Adams",
+        song: "(Everything I Do) I Do It For You",
+},
+    {
+        artist: "Adele",
+        song: "Rolling In The Deep",
+},
+    {
+        artist: "Frank Sinatra",
+        song: "New York, New York",
+}
+];
+
+module.exports = songs;
