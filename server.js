af3f151601f8569b907bda51e24ec47e0b557bb0
bgirlcoolcat <evansb21@gmail.com>
2017-10-25 21:06:21 +0100

    api key hidden in dotenv file for security

--- server.js
+++ server.js
@@ -3,24 +3,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const axios = require('axios');
 const songs = require('./songList');
+require('dotenv').config({path: './dev/apiKey.env'});
+
+const apiKey = process.env.API_KEY;
 
 app.listen(3000);
 
 app.use(bodyParser.json())
 
-// app.get("/", function(req, res) {
-//     const url = 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao&apikey=c6af8e74da168c2f810eab97f6a8f603'; 
-    
-//     return axios
-//         .get(url)
-//         .then(response => {
-//             // res.send(response.data.message.body)
-//             res.send(response.data.message.body.lyrics.lyrics_body)
-//         }
-//         )
-//         .catch(e => res.send(e.message));
-
-// })
 
 let title = "";
 let singer= "";
@@ -36,8 +26,9 @@ app.get("/random_song", function(req, res) {
     
     songRandomiser(songs);
     
-    const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${title}&q_artist=${singer}&apikey=c6af8e74da168c2f810eab97f6a8f603`; 
-    
+    const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${title}&q_artist=${singer}&apikey=${apiKey}`; 
+    // const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${title}&q_artist=${singer}&apikey=c6af8e74da168c2f810eab97f6a8f603`;
+
     return axios
         .get(url)
         .then(response => {
@@ -46,12 +37,12 @@ app.get("/random_song", function(req, res) {
 
             //take first 3 new lines 
             let lyricsArray = lyrics.split("\n");
-            let lyricsOutput = lyricsArray.slice(0, 3).join("/n");
+            let lyricsOutput = lyricsArray.slice(0, 3).join(". ")+".";
 
             res.send({
-                title: title,
-                singer: singer,
-                lyricsOutput: lyricsOutput 
+                song: title,
+                artist: singer,
+                lyrics: lyricsOutput 
             })
         }
         )
--- songList.js
+++ songList.js
@@ -5,40 +5,108 @@ const songs = [
     }, 
     {
         artist: "David Bowie",
-        song: "Starman",
-},
+        song: "Space Oddity",
+    },
     {
         artist: "Taylor Swift",
         song: "Shake It Off",
-},
+    },
     {
         artist: "Take That",
         song: "Rule the World",
-},
+    },
     {
         artist: "LMFAO",
         song: "Rock the World",
-},
+    },
     {
         artist: "The Beatles",
         song: "Hey Jude",
-},
+    },
     {
         artist: "Diana Ross and Lionel Ritchie", 
         song: "Endless Love",
- },
+    },
     {
         artist: "Bryan Adams",
         song: "(Everything I Do) I Do It For You",
-},
+    },
     {
         artist: "Adele",
         song: "Rolling In The Deep",
-},
+    },
     {
         artist: "Frank Sinatra",
         song: "New York, New York",
-}
+    },
+    {
+        artist: "Madonna",
+        song: "Music",
+    },
+    {
+        artist: "Elvis",
+        song: "Jailhouse Rock",
+    },
+    {
+        artist: "Cher",
+        song: "Believe",
+    },
+    {
+        artist: "Irene Carr",
+        song: "Fame",
+    },
+    {
+        artist: "Ricky Martin",
+        song: "Livin' La Vida Loca",
+    },
+    {
+        artist: "Nirvana",
+        song: "Smells Like Teen Spirit",
+    },
+    {
+        artist: "Aerosmith",
+        song: "Walk This Way",
+    },
+    {
+        artist: "Spice Girls",
+        song: "Wannabe",
+    },
+    {
+        artist: "Michael Jackson",
+        song: "Thriller",
+    },
+    {
+        artist: "Public Enemy",
+        song: "Don't Believe The Hype",
+    },
+    {
+        artist: "Beastie Boys",
+        song: "Fight For Your Right",
+    },
+    {
+        artist: "Bob Dylan",
+        song: "Subterranean Homesick Blues",
+    },
+    {
+        artist: "Britney Spears",
+        song: "Oops!...I Did It Again",
+    },
+    {
+        artist: "Drake",
+        song: "Hold On We're Going Home",
+    },
+    {
+        artist: "Calvin Harris",
+        song: "Summer",
+    },
+    {
+        artist: "Amerie",
+        song: "One Thing",
+    },
+    {
+        artist: "50 Cent",
+        song: "P.I.M.P",
+    },
 ];
 
 module.exports = songs;
