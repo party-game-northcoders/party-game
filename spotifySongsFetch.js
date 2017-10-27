10e16fa2a5adb06880bd83a7671eac29adb97f07
ffraniel <francis.whitehead@googlemail.com>
2017-10-27 13:12:58 +0100

    sorted topArtists call to sort on popularity of artists and return 30 topmost

--- spotifySongsFetch.js
+++ spotifySongsFetch.js
@@ -1,58 +1,68 @@
 // basic server
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const axios = require('axios');
-app.use(bodyParser.json())
-app.listen(3000)
+const bodyParser = require("body-parser");
+const axios = require("axios");
+app.use(bodyParser.json());
+app.listen(3000);
 
 const artistsForGame = [];
 const songsForGame = [];
 // authentication passed through via headers
 const spotifyHeaders = {
-    headers: {
-        Accept: 'application/json',
-        Authorization:"Bearer BQDwOI4wVn9NMP_WjYoKZ_vIqr14_7i8BqDDjiTCJzHu652PT96cGqQf9Ix15t4U-ZrVoh5LtIJED9DAzC1R6cBH7E1vJdFpYjnDeaO_cyOCQxmfIsSO11wm4VJnLA5qi9l7plXH1jX-OFBDCiSsItxbDS4JdWSYVI4i5rDedHZCNWRfOu5mHrB8fmopHAr9X0AMOBGO2GbjRRx-Bc3z1UNEwjazUTJ7A2SQNLGWk7bXSj3jeA_tFUJzmNfTVa5_oCwFeFE1qqKizJcJtiQnvONWZYipWs7b0f0gN3bpz93_7EPDWNwigE4GJ47QinAWSS7MFw"
-    }  
+  headers: {
+    Accept: "application/json",
+    Authorization:
+      "Bearer BQCjotcS4ALVz8s9lewnLPKKRCmwG7P7fFXTLHgdJjcsHn7hWCA04uYLjptMv9MfLUN3K0gICMahCcgq367rWl27n8Y6WCkgU8sju8d3OuLelM_Q2x5etdPJGoukIYg90ZwvmBY932gwLI4v4j8rSCwONNvxuvMPQF7rFR9zlM78Bvx4Wbhsz-eKC_Yipb8OAXOOch9MN6KR-Dbtq7xunA3X1WTeUKCY9H9ahZgnxlFVvNrj9jVJFtEMja9BEef7ZzaoXm19nl0r8o1lcGh_rVxNVJjvujW-lSzYA2M9WEZMYv3Fow5Z0s2TTvYFOSykaXR0rA"
+  }
 };
 
 // fetch user's top artists or songs - either do all the songs and select on popularity or all the artists and same
-app.get('/topArtists', function (req, res) {
-    return axios.get("https://api.spotify.com/v1/me/top/artists", spotifyHeaders) 
-    .then((response)=>{
-        response.data.items.map(function (artist) {
-            artistsForGame.push({
-                name: artist.name, 
-                id: artist.id, 
-                popularity:artist.popularity
-            })
-        })
-        res.send(artistsForGame)
+app.get("/topArtists", function(req, res) {
+  return axios
+    .get("https://api.spotify.com/v1/me/top/artists?limit=30", spotifyHeaders)
+    .then(response => {
+      response.data.items.map(function(artist) {
+        artistsForGame.push({
+          name: artist.name,
+          id: artist.id,
+          popularity: artist.popularity
+        });
+      });
+      artistsForGame.sort((a, b) => {
+        return b.popularity - a.popularity;
+      });
+      res.send(artistsForGame.slice(0, 30));
     })
-    .catch((err) => {
-        throw err;
+    .catch(err => {
+      throw err;
+    });
+});
+
+app.get("/topArtistsSongs", function(req, res) {
+  return axios
+    .get(
+      "https://api.spotify.com/v1/artists/5K4W6rqBFWDnAN6FQUkS6x/top-tracks?country=GB",
+      spotifyHeaders
+    )
+    .then(response => {
+      response.data.tracks.map(function(track) {
+        songsForGame.push({
+          name: track.name,
+          id: track.id,
+          popularity: track.popularity,
+          artist: track.artists[0].name
+        });
+      });
+      songsForGame.sort((a, b) => {
+        return b.popularity - a.popularity;
+      });
+      res.send(songsForGame.slice(0, 5));
     })
-})
-
-
-app.get('/topArtistsSongs', function (req, res){
-    return axios.get("https://api.spotify.com/v1/artists/5K4W6rqBFWDnAN6FQUkS6x/top-tracks?country=GB", spotifyHeaders)
-    .then((response)=>{
-        response.data.tracks.map(function (track){
-            songsForGame.push({
-                name:track.name,
-                id:track.id, 
-                popularity:track.popularity,
-                artist: track.artists[0].name
-            })
-        })
-        res.send(songsForGame)
-;    })
-    .catch((err) => {
-        throw err
-    })
-})
-
+    .catch(err => {
+      throw err;
+    });
+});
 
 // -- Artist generator --
 // the results is the whole 50 artists - from here we will take the most popular 30 (if available) artist IDs
@@ -62,6 +72,5 @@ app.get('/topArtistsSongs', function (req, res){
 
 //  -- This output informs music match search. There is a need for another step before the music match search.
 // - using artist id find out the artists popular songs 10 each?
-// - get popular songs from those artists 
+// - get popular songs from those artists
 //  and top fifty songs on spotify
-
