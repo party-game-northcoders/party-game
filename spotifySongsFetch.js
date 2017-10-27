f189da8cd02c21fd5906cd989f2a5bc7446f4c35
ffraniel <francis.whitehead@googlemail.com>
2017-10-27 11:05:56 +0100

    Ability to fetch top artists of user

---
+++ spotifySongsFetch.js
@@ -0,0 +1,40 @@
+// basic server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+app.use(bodyParser.json())
+app.listen(3000)
+
+
+// authentication passed through via headers
+const spotifyHeaders = {
+    headers: {
+        Accept: 'application/json',
+        Authorization:"Bearer BQCNORERKAFMBzLSoCx8NutpkT81AXFCAw5w4gg17PrP10Gq2cEHCZjgAu25LArqRVgUpGc-qe-EtHwAEm3rEd8Wu0ziePwGrWo649tyxFjz6eL543xqmyh5eE4s0DYXUC3qD3lQFzYhER7vaTY1otA"
+    }  
+};
+
+// fetch user's top artists or songs - either do all the songs and select on popularity or all the artists and same
+app.get('/', function (req, res) {
+    return axios.get("https://api.spotify.com/v1/me/top/artists", spotifyHeaders) 
+    .then((response)=>{
+        res.send(response.data)
+    })
+    .catch((err) => {
+        throw err;
+    })
+})
+
+
+// -- Artist generator --
+// the results is the whole 50 artists - from here we will take the most popular 30 (if available) artist IDs
+
+//  response.data.items this is an array
+// response.data.items[i] all things available here artist names, popularity, id
+
+//  -- This output informs music match search. There is a need for another step before the music match search.
+// - using artist id find out the artists popular songs 10 each?
+// - get popular songs from those artists 
+//  and top fifty songs on spotify
+
